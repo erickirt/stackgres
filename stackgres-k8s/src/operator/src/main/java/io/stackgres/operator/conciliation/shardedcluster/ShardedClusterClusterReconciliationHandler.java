846c887b1af8fd660a66957b75bc4ab0fcd61b42
Matteo Melli <matteom@ongres.com>
2026-04-20 14:09:45 +0200

    feat: just downscale a cluster to 0 when removed from a sharded cluster

--- stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/conciliation/shardedcluster/ShardedClusterClusterReconciliationHandler.java
+++ stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/conciliation/shardedcluster/ShardedClusterClusterReconciliationHandler.java
@@ -6,10 +6,10 @@
 package io.stackgres.operator.conciliation.shardedcluster;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.stackgres.common.crd.sgcluster.StackGresCluster;
 import io.stackgres.common.crd.sgshardedcluster.StackGresShardedCluster;
-import io.stackgres.operator.conciliation.FireAndForgetReconciliationHandler;
-import io.stackgres.operator.conciliation.ReconciliationHandler;
+import io.stackgres.operator.conciliation.AbstractReconciliationHandler;
 import io.stackgres.operator.conciliation.ReconciliationScope;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -19,45 +19,14 @@ import org.slf4j.LoggerFactory;
 @ReconciliationScope(value = StackGresShardedCluster.class, kind = StackGresCluster.KIND)
 @ApplicationScoped
 public class ShardedClusterClusterReconciliationHandler
-    implements ReconciliationHandler<StackGresShardedCluster> {
+    extends AbstractReconciliationHandler<StackGresShardedCluster> {
 
   protected static final Logger LOGGER =
-      LoggerFactory.getLogger(FireAndForgetReconciliationHandler.class);
-
-  private final ReconciliationHandler<StackGresShardedCluster> handler;
+      LoggerFactory.getLogger(ShardedClusterClusterReconciliationHandler.class);
 
   @Inject
-  public ShardedClusterClusterReconciliationHandler(
-      ReconciliationHandler<StackGresShardedCluster> handler) {
-    this.handler = handler;
-  }
-
-  @Override
-  public final HasMetadata create(StackGresShardedCluster context, HasMetadata resource) {
-    return doCreate(context, resource);
-  }
-
-  protected HasMetadata doCreate(StackGresShardedCluster context, HasMetadata resource) {
-    return handler.create(context, resource);
-  }
-
-  @Override
-  public final HasMetadata patch(StackGresShardedCluster context, HasMetadata newResource,
-      HasMetadata oldResource) {
-    return doPatch(context, newResource, oldResource);
-  }
-
-  protected HasMetadata doPatch(StackGresShardedCluster context, HasMetadata newResource, HasMetadata oldResource) {
-    return handler.patch(context, newResource, oldResource);
-  }
-
-  @Override
-  public final HasMetadata replace(StackGresShardedCluster context, HasMetadata resource) {
-    return doReplace(context, resource);
-  }
-
-  protected HasMetadata doReplace(StackGresShardedCluster context, HasMetadata resource) {
-    return handler.replace(context, resource);
+  public ShardedClusterClusterReconciliationHandler(KubernetesClient client) {
+    super(client);
   }
 
   @Override
@@ -77,10 +46,9 @@ public class ShardedClusterClusterReconciliationHandler
   }
 
   private void doDownscaleToZero(StackGresShardedCluster context, StackGresCluster cluster) {
-    if (cluster.getSpec() != null) {
-      cluster.getSpec().setInstances(0);
-    }
-    handler.patch(context, cluster, cluster);
+    LOGGER.debug("Scaling " + StackGresCluster.KIND + " " + cluster.getMetadata().getName() +  " to 0 since deleted");
+    cluster.getSpec().setInstances(0);
+    patch(context, cluster, cluster);
   }
 
 }
